7e37c97dffbcbc9bf59e66964acf9fd47d087f64
andreimoraru67 <andreimoraru3496@gmail.com>
2022-11-29 18:03:09 +0200

    Revert "Tema 22.11.22"
    This reverts commit f8e94be20e409b04226462f66451affdc1c6f6a2.

--- m01s03/ex01/app.js
+++ m01s03/ex01/app.js
@@ -33,22 +33,3 @@ curent (un proces numit hardcodare)
 `);
 
 console.log((2022 - object.age).toString());
-
-console.warn(`
-Afiseaza fraza “xxx yyy are xx ani.”
-in consola folosind operatorul de concatenare
-`);
-console.log(
-  object.name + ' ' + object.surname + ' are ' + object.age + ' ani.',
-);
-
-console.warn(`
-Afiseaza fraza “xxx s-a nascut in aaaa.”
-Foloseste tot anul curent pentru a scadea varsta.
-`);
-console.log(
-  object.name +
-    ' s-a nascut in ' +
-    (year.getFullYear() - object.age).toString() +
-    '.',
-);
