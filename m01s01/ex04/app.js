f8e94be20e409b04226462f66451affdc1c6f6a2
andreimoraru67 <andreimoraru3496@gmail.com>
2022-11-29 17:58:53 +0200

    Tema 22.11.22

---
+++ m01s01/ex04/app.js
@@ -0,0 +1,17 @@
+var inputRadius = document.getElementById('radius');
+var elementResult = document.getElementById('result');
+var form = document.querySelector('form');
+
+form.addEventListener(
+  'submit',
+  function (event) {
+    var radius = inputRadius.value || 0;
+    var result = 0;
+
+    result = Math.pow(radius, 2) * Math.PI;
+    // result = radius * radius * Math.PI;
+
+    elementResult.innerText = result;
+  },
+  false,
+);
