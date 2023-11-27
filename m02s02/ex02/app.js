6a3019c430a1196efb85fec67ffff85504cd2c1c
andreimoraru67 <andreimoraru3496@gmail.com>
2023-11-27 14:08:09 +0200

    Tema 2 Februarie

---
+++ m02s02/ex02/app.js
@@ -0,0 +1,379 @@
+const $personForm = $('#personForm')
+  .on('submit', function (event) {
+    event.preventDefault();
+    const $form = $(this);
+    const $nameInput = $form.find('input[name="name"]');
+    const $surnameInput = $form.find('input[name="surname"]');
+    const $ageInput = $form.find('input[name="age"]');
+    const $skillInputs = $form.find('input[name^="skill-"]');
+    const $petInputs = $form.find('input[name^="pet-"]');
+    const $friendInputs = $form.find('input[name^="friend-"]');
+
+    const skills = [];
+    $skillInputs.each(function () {
+      const $skilInput = $(this);
+
+      skills.push($skilInput.val());
+    });
+
+    const pets = [];
+    $petInputs.each(function () {
+      const $input = $(this);
+      const name = $input.prop('name');
+      const petValue = name.split('-')[1];
+      const parts = petValue.split(', ');
+      const pet = {
+        name: parts[0],
+        species: parts[1],
+        age: parts[2],
+      };
+
+      pets.push(pet);
+    });
+
+    const friends = [];
+    $friendInputs.each(function () {
+      const $input = $(this);
+      const name = $input.prop('name');
+      const friendValue = name.split('-')[1];
+      const parts = friendValue.split(', ');
+      const friend = {
+        name: parts[0],
+        surname: parts[1],
+        age: parts[2],
+      };
+
+      pets.push(friend);
+    });
+
+    const person = {
+      name: $nameInput.val(),
+      surname: $surnameInput.val(),
+      age: $ageInput.val(),
+      skills,
+      pets,
+      friends,
+    };
+
+    $form.trigger('reset');
+    $form.find('.skills-ul').remove();
+    $form.find('.pet-preview-ul').remove();
+    $form.find('.friend-preview-ul').remove();
+    $form.next().remove();
+
+    $form.after(displayPerson(person));
+  })
+  .on('click', '.delete-skill-button', function () {
+    $(this).parent().remove();
+  })
+  .on('click', '.edit-skill-button', function () {
+    const $editSkillButton = $(this);
+
+    $editSkillButton.siblings('.delete-skill-button').hide();
+    $editSkillButton.siblings('.skill-label').hide();
+    $editSkillButton.siblings('input[name^="skill-"]').prop('type', 'text');
+    $editSkillButton.siblings('.cancel-edit-skill-button').show();
+    $editSkillButton.siblings('.save-skill-button').show();
+    $editSkillButton.hide();
+  })
+  .on('click', '.cancel-edit-skill-button', function cancelEditSkill() {
+    const $cancelEditSkillButton = $(this).hide();
+
+    $cancelEditSkillButton.siblings('.delete-skill-button').show();
+    $cancelEditSkillButton.siblings('.edit-skill-button').show();
+    $cancelEditSkillButton
+      .siblings('input[name^="skill-"]')
+      .prop('type', 'hidden');
+    $cancelEditSkillButton.siblings('.skill-label').show();
+    $cancelEditSkillButton.siblings('.save-skill-button').hide();
+  })
+  .on('click', '.save-skill-button', function saveSkill() {
+    const $saveSkillButton = $(this).hide();
+    const $skillInput = $saveSkillButton
+      .siblings('input[name^="skill-"]')
+      .prop('type', 'hidden');
+    const newSkill = $skillInput.val();
+    const skillParts = $skillInput.prop('name').split('-');
+    skillParts[1] = newSkill;
+
+    $skillInput.prop('name', skillParts.join('-'));
+
+    $saveSkillButton.siblings('.delete-skill-button').show();
+    $saveSkillButton.siblings('.edit-skill-button').show();
+    $saveSkillButton.siblings('.skill-label').text(newSkill).show();
+    $saveSkillButton.siblings('.cancel-edit-skill-button').hide();
+  })
+  .on('click', '.create-pet-button', function () {
+    const $createPetButton = $(this);
+    const $inputs = $createPetButton.siblings('input[name]');
+    const pet = {};
+
+    $inputs.each(function () {
+      const $input = $(this);
+      const value = $input.val();
+      const key = $input.prop('name').split('-').pop();
+
+      pet[key] = value;
+    });
+
+    $inputs.val('');
+
+    $createPetButton.after(renderPetUl(pet));
+  })
+  .on('click', '.create-friend-button', function () {
+    const $createFriendButton = $(this);
+    const $inputs = $createFriendButton.siblings('input[name]');
+    const friend = {};
+
+    $inputs.each(function () {
+      const $input = $(this);
+      const value = $input.val();
+      const key = $input.prop('name').split('-').pop();
+
+      friend[key] = value;
+    });
+
+    $inputs.val('');
+
+    $createFriendButton.after(renderFriendUl(friend));
+  });
+
+$('#has-pets').on('click', function () {
+  const $checkbox = $(this);
+  const $fieldset = $checkbox.next().next();
+  const checked = $checkbox.prop('checked') === true;
+
+  if (checked === true) {
+    $fieldset.slideDown();
+  } else {
+    $fieldset.slideUp();
+  }
+});
+
+const $skillButton = $('#skill')
+  .next()
+  .on('click', function () {
+    const $addSkillButton = $(this);
+    const $skillInput = $addSkillButton.prev();
+    const skillValue = $skillInput.val();
+
+    $skillInput.val('').focus();
+
+    if (skillValue.trim().length < 3) {
+      return;
+    }
+
+    const $skillsUl = renderSkillsUl(skillValue);
+    $addSkillButton.after($skillsUl);
+  });
+
+function renderSkillsUl(skill = '') {
+  const cssClass = 'skills-ul';
+  let $skillsUl = $(`.${cssClass}`);
+
+  if ($skillsUl.length === 0) {
+    $skillsUl = $('<ul>', {
+      class: cssClass,
+    });
+  }
+
+  const $skillLi = $('<li>')
+    .appendTo($skillsUl)
+    .append(
+      $('<span>', {
+        text: skill,
+        class: 'skill-label',
+      }),
+    );
+
+  $('<input>', {
+    value: skill,
+    name: `skill-${skill}`,
+    type: 'hidden',
+  }).appendTo($skillLi);
+
+  $('<button>', {
+    type: 'button',
+    text: 'Delete',
+    class: 'delete-skill-button',
+  }).appendTo($skillLi);
+
+  $('<button>', {
+    type: 'button',
+    text: 'Edit',
+    class: 'edit-skill-button',
+  }).appendTo($skillLi);
+
+  $('<button>', {
+    type: 'button',
+    text: 'Cancel',
+    class: 'cancel-edit-skill-button',
+  })
+    .hide()
+    .appendTo($skillLi);
+
+  $('<button>', {
+    type: 'button',
+    text: 'Save',
+    class: 'save-skill-button',
+  })
+    .hide()
+    .appendTo($skillLi);
+
+  return $skillsUl;
+}
+
+function renderPetUl(pet = {}) {
+  const ulClass = 'pet-preview-ul';
+  let $ul = $(`.${ulClass}`);
+
+  let petString = '';
+  Object.keys(pet).forEach(function (keyName, index, keys) {
+    const value = pet[keyName];
+    let punctuation = ', ';
+
+    if (keys.length - 1 === index) {
+      punctuation = '';
+    }
+
+    petString += `${value}${punctuation}`;
+  });
+
+  if ($ul.length === 0) {
+    $ul = $('<ul>', {
+      class: ulClass,
+    });
+  }
+
+  const $li = $('<li>')
+    .append(
+      $('<span>', {
+        text: petString,
+      }),
+    )
+    .append(
+      $('<input>', {
+        type: 'hidden',
+        value: petString,
+        name: `pet-${petString}`,
+      }),
+    );
+
+  $ul.append($li);
+
+  return $ul;
+}
+
+function renderFriendUl(friend = {}) {
+  const ulClass = 'friend-preview-ul';
+  let $ul = $(`.${ulClass}`);
+
+  let friendString = '';
+  Object.keys(friend).forEach(function (keyName, index, keys) {
+    const value = friend[keyName];
+    let punctuation = ', ';
+
+    if (keys.length - 1 === index) {
+      punctuation = '';
+    }
+
+    friendString += `${value}${punctuation}`;
+  });
+
+  if ($ul.length === 0) {
+    $ul = $('<ul>', {
+      class: ulClass,
+    });
+  }
+
+  const $li = $('<li>')
+    .append(
+      $('<span>', {
+        text: friendString,
+      }),
+    )
+    .append(
+      $('<input>', {
+        type: 'hidden',
+        value: friendString,
+        name: `friend-${friendString}`,
+      }),
+    );
+
+  $ul.append($li);
+
+  return $ul;
+}
+
+function displayPerson(person) {
+  const $personDisplay = $('<div>');
+
+  $personDisplay
+    .append(displayPersonDetails(person))
+    .append(displayPersonSkills(person))
+    .append(displayPet(person))
+    .append(displayFriend(person));
+
+  return $personDisplay;
+}
+
+function displayPet(person) {
+  const $petDisplay = $('<div>');
+
+  person.pets.forEach(function (pet) {
+    $('<p>', {
+      text: `${pet.name}, ${pet.species}, ${pet.age}`,
+    }).appendTo($petDisplay);
+  });
+
+  return $petDisplay;
+}
+
+function displayFriend(person) {
+  const $friendDisplay = $('<div>');
+
+  person.friends.forEach(function (friend) {
+    $('<p>', {
+      text: `${friend.name}, ${friend.surname}, ${friend.age}`,
+    }).appendTo($friendDisplay);
+  });
+
+  return $friendDisplay;
+}
+
+function displayPersonDetails(person) {
+  const $personDetails = $('<div>');
+  $('<h1>', {
+    text: `${person.name} ${person.surname}`,
+  }).appendTo($personDetails);
+
+  $('<p>', {
+    text: `Age: ${person.age}`,
+  }).appendTo($personDetails);
+
+  return $personDetails;
+}
+
+function displayPersonSkills(person) {
+  const skills = person.skills;
+
+  if (skills.length < 1) {
+    return null;
+  }
+
+  const $container = $('<div>');
+
+  $('<h2>', {
+    text: 'Skills',
+  }).appendTo($container);
+
+  const $ul = $('<ul>').appendTo($container);
+
+  skills.forEach(function (skill) {
+    $('<li>', {
+      text: skill,
+    }).appendTo($ul);
+  });
+
+  return $container;
+}
